b9ec54ea00a1018f3c01cd176d73c711cd43d0b1
viktor <vice.andre@gmail.com>
2025-09-25 14:30:10 +0200

    1

--- functions/.eslintrc.cjs
+++ functions/.eslintrc.cjs
@@ -13,10 +13,10 @@ module.exports = {
   },
   rules: {
     "quotes": ["error", "double"],
-    "max-len": ["error", { "code": 120 }], // Tillåt lite längre rader
+    "max-len": ["error", { "code": 120 }],
     "object-curly-spacing": ["error", "always"],
     "indent": ["error", 2],
   },
-  // Denna regel talar om att BARA ignorera node_modules
+  // This rule ensures ESLint only ignores the node_modules folder
   ignorePatterns: ["node_modules/"],
 };
